d7b467879527a4fc01ea90ad0aaaf92e38a2a617
alexandre-hazan <alexandre.hazan@edu.esiee.fr>
2024-11-15 14:42:05 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 #### Fonction secondaire
-
-
+"""exercice 05-palindrome """
+import string
+TABLE1 = str.maketrans ("éèëêàçô", "eeeeaco")
+TABLE2 =str.maketrans('','',string.punctuation)
 def ispalindrome(p):
-
-    # votre code ici
-    
+    """fonction qui vérifie qu'un mot est bien un palindrome """
+    if len(p)<=1 :
+        return True
+    p = p.lower().translate(TABLE1).translate(TABLE2).replace(" ", "")
+    if p[0]==p[-1]:
+        return ispalindrome(p[1:-1])
     return False
-
 #### Fonction principale
 
 
 def main():
+    """ execute la fonction ispalindrome"""
 
     # vos appels à la fonction secondaire ici
 
